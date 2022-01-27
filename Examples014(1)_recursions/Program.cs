0ca09b13925abd9ec72cf6502e236979ad6dc522
ZauYana <zaulskayana@gmail.com>
2022-01-27 18:09:26 +0300

    lection 25.01.2022

---
+++ Examples014(1)_recursions/Program.cs
@@ -0,0 +1,31 @@
+﻿//  собрать строку с числами от a до b, a<= b
+
+string NumbersFor(int a, int b)
+{
+    string result = String.Empty;
+    for (int i = a; i <= b; i++)
+    {
+        result += $"{i}";
+    }
+    return result;
+}
+
+Console.WriteLine (NumbersFor(2,10));
+Console.WriteLine ("Вывод без рекурсии");
+
+string NumbersRec (int a, int b)
+{
+    if (a<=b) return $"{a}" + NumbersRec (a+1,b);
+    else return String.Empty;
+}
+Console.WriteLine (NumbersRec(2,10));
+Console.WriteLine ("Вывод c рекурсией");
+
+string NumbersRec1 (int a, int b)
+{
+    if (a<b) return $"{a}" + NumbersRec1 (a+1,b);
+    else return $"{b}";
+}
+Console.WriteLine (NumbersRec1(2,10));
+Console.WriteLine ("Вывод c рекурсией, на одну рекурсию меньше");
+
