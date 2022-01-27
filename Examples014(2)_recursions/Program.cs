a12fdbc21e546076b52a18fd761f3d2a5ebdb6f5
ZauYana <zaulskayana@gmail.com>
2022-01-27 18:19:44 +0300

    simple exm of recursions

---
+++ Examples014(2)_recursions/Program.cs
@@ -0,0 +1,13 @@
+﻿// вывести строку с числами от a до b, a>=b
+
+string NumbersFor1(int a, int b)
+{
+    string result = String.Empty;
+    for (int i = a; i >= b; i--)
+    {
+        result += $"{i}";
+    }
+    return result;
+}
+
+Console.WriteLine(NumbersFor1(1, 10));
