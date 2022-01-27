a4c5dd734e130af8ef14877417bc02388b840308
ZauYana <zaulskayana@gmail.com>
2022-01-27 19:41:24 +0300

    recursion (на уменьшение)

--- Examples014(2)_recursions/Program.cs
+++ Examples014(2)_recursions/Program.cs
@@ -1,13 +1,36 @@
 ﻿// вывести строку с числами от a до b, a>=b
 
-string NumbersFor1(int a, int b)
+string NumbersFor(int a, int b)
 {
     string result = String.Empty;
-    for (int i = a; i >= b; i--)
+    for (int i = b; i >= a; i--)
     {
         result += $"{i}";
     }
     return result;
 }
 
-Console.WriteLine(NumbersFor1(1, 10));
+// void PrintNumbersFor (string numbers)
+// {
+//     for (int i = 0; i<numbers.Length; i++)
+//     {
+//         Console.Write (numbers[i]);
+//     }
+// }
+
+// string resultOfNumbersFor = NumbersFor (10,1);
+// PrintNumbersFor (resultOfNumbersFor);
+
+Console.WriteLine (NumbersFor(1,10));
+Console.WriteLine ("Цикл на уменьшение");
+
+
+string NumbersRec(int a, int b)
+{
+    if (a <= b) return NumbersRec(a + 1, b) + $"{a}";
+    else return String.Empty;
+
+}
+
+Console.WriteLine(NumbersRec(1, 10));
+Console.WriteLine ("Цикл на уменьшение с рекурсией");
