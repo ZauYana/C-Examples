899f875bfa832c5ef4d111a0020dfd1b1d4011a8
ZauYana <zaulskayana@gmail.com>
2022-01-29 16:47:53 +0300

    task Sum from 1 to N

--- Examples014(3)_recursions/Program.cs
+++ Examples014(3)_recursions/Program.cs
@@ -1,3 +1,24 @@
 ﻿// сумма чисел от 1 до n 
 
 
+int SumFor(int n)
+{
+    int result = 0;
+    for (int i = 1; i <= n; i++) result += i;
+    return result;
+}
+
+int resultSumFor = SumFor(10);
+Console.WriteLine(resultSumFor);
+Console.WriteLine ("метод через for");
+Console.WriteLine();
+
+int SumRec(int n)
+{
+    if (n == 0) return 0;
+    else return n + SumRec(n-1);
+}
+
+int resultSumRec = SumRec (10);
+Console.WriteLine (resultSumRec);
+Console.WriteLine ("метод через recursion");
