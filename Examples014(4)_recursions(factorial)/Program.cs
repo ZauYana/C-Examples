ff9d94c46a242d4c6bdc20a37beb13047432f69e
ZauYana <zaulskayana@gmail.com>
2022-01-30 12:14:25 +0300

    recursions - factorial

---
+++ Examples014(4)_recursions(factorial)/Program.cs
@@ -0,0 +1,24 @@
+﻿// Factorial
+
+int FactorialFor(int n)
+{
+    int result = 1;
+    for (int i = 1; i <= n; i++) result *= i;
+    return result;
+}
+
+int resultFactorialFor = FactorialFor(5);
+Console.WriteLine(resultFactorialFor);
+Console.WriteLine ();
+
+
+int FactorialRec(int n)
+{
+    if (n == 1) return 1;
+    if (n == 0) return 1;
+    else return n*FactorialRec(n-1);
+}
+
+int resultFactorialRec = FactorialRec(5);
+Console.WriteLine(resultFactorialRec);
+Console.WriteLine ();
